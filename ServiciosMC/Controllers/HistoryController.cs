1f30fba4841b2b00f499bb415563c91fc297e895
proegcm <egcmpro@gmail.com>
2024-09-11 01:20:34 -0600

    ADD - Estilos - tablas

--- ServiciosMC/Controllers/HistoryController.cs
+++ ServiciosMC/Controllers/HistoryController.cs
@@ -32,6 +32,7 @@ namespace ServiciosMC.Controllers
         {
             return View();
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
